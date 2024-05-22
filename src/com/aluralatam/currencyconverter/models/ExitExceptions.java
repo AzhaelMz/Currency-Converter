70034a840ad09213a7fe4c63ec8609cd04942740
AzhaelMz <158383050+AzhaelMz@users.noreply.github.com>
2024-05-22 11:19:24 -0500

    Delete src/com/aluralatam/currencyconverter directory

--- src/com/aluralatam/currencyconverter/models/ExitExceptions.java
+++
@@ -1,7 +0,0 @@
-package com.aluralatam.currencyconverter.models;
-
-public class ExitExceptions extends RuntimeException {
-    public ExitExceptions(String message){
-        super(message);
-    }
-}
--- src/com/aluralatam/currencyconverter/models/InvalidOptionExcep.java
+++
@@ -1,7 +0,0 @@
-package com.aluralatam.currencyconverter.models;
-
-public class InvalidOptionExcep extends Exception{
-    public InvalidOptionExcep(String message){
-        super(message);
-    }
-}
